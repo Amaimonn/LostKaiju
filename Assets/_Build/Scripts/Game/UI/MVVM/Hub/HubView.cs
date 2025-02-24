7e6cd39f05fdb2a290bb7862091aabe81ad2de7a
Amaimonn <150851609+Amaimonn@users.noreply.github.com>
2025-02-24 17:38:12 +0300

    HeroSelectionView screen binding and showing

--- Assets/_Build/Scripts/Game/UI/MVVM/Hub/HubView.cs
+++ Assets/_Build/Scripts/Game/UI/MVVM/Hub/HubView.cs
@@ -8,18 +8,28 @@ namespace LostKaiju.Game.UI.MVVM.Hub
     public class HubView : ToolkitView<HubViewModel>
     {
         [SerializeField] private string _openMissionsElementName;
+        [SerializeField] private string _openHeroSelectionButtonName;
 
         private VisualElement _openMissionsElement;
+        private Button _openHeroSelectionButton;
 
         protected override void OnBind(HubViewModel viewModel)
         {
             _openMissionsElement = Root.Q<VisualElement>(name: _openMissionsElementName);
             _openMissionsElement.RegisterCallback<ClickEvent>(OpenMissions);
+
+            _openHeroSelectionButton = Root.Q<Button>(name: _openHeroSelectionButtonName);
+            _openHeroSelectionButton.RegisterCallback<ClickEvent>(OpenHeroSelection);
         }
 
         private void OpenMissions(ClickEvent clickEvent)
         {
             ViewModel.OpenMissions();
+        }
+
+        private void OpenHeroSelection(ClickEvent clickEvent)
+        {
+            ViewModel.OpenHeroSelection();
         }
     }
 }
--- Assets/_Build/Scripts/Game/UI/MVVM/Hub/HubViewModel.cs
+++ Assets/_Build/Scripts/Game/UI/MVVM/Hub/HubViewModel.cs
@@ -1,10 +1,10 @@
 using System;
+using System.Threading.Tasks;
 using UnityEngine;
 using R3;
 
 using LostKaiju.Boilerplates.UI.MVVM;
 using LostKaiju.Game.GameData.Campaign;
-using System.Threading.Tasks;
 
 namespace LostKaiju.Game.UI.MVVM.Hub
 {
@@ -14,7 +14,9 @@ namespace LostKaiju.Game.UI.MVVM.Hub
         private readonly Func<Task<CampaignModel>> _campaignModelFactory;
         private readonly IRootUIBinder _rootUIBinder;
         private bool _isMissionsOpened = false;
+        private bool _isHeroSelectionOpened = false;
         private const string CAMPAIGN_NAVIGATION_VIEW_PATH = "UI/Hub/CampaignNavigationView";
+        private const string HERO_SELECTION_VIEW_PATH = "UI/Hub/HeroSelectionView";
         
         public HubViewModel(Subject<Unit> exitSubject, Func<Task<CampaignModel>> campaignModelFactory, 
             IRootUIBinder rootUIBinder)
@@ -37,10 +39,12 @@ namespace LostKaiju.Game.UI.MVVM.Hub
                 _rootUIBinder.ClearView(campaignView);
                 _isMissionsOpened = false;
             });
+
             BindCampaignAsync();
             campaignView.Bind(campaignViewModel);
             _rootUIBinder.AddView(campaignView);
             campaignViewModel.Open();
+            
             _isMissionsOpened = true;
 
             async void BindCampaignAsync()
@@ -51,6 +55,27 @@ namespace LostKaiju.Game.UI.MVVM.Hub
                     campaignViewModel.Bind(campaignModel);
                 }
             }
+        }
+
+        public void OpenHeroSelection()
+        {
+            if (_isHeroSelectionOpened)
+                return;
+            
+            var heroSelectionViewPrefab = Resources.Load<HeroSelectionView>(HERO_SELECTION_VIEW_PATH);
+            var heroSelectionView = UnityEngine.Object.Instantiate(heroSelectionViewPrefab);
+
+            var heroSelectionViewModel = new HeroSelectionViewModel();
+            heroSelectionViewModel.OnClosingCompleted.Subscribe(_ =>  {
+                _rootUIBinder.ClearView(heroSelectionView);
+                _isHeroSelectionOpened = false;
+            });
+
+            heroSelectionView.Bind(heroSelectionViewModel);
+            _rootUIBinder.AddView(heroSelectionView);
+            heroSelectionViewModel.Open();
+
+            _isHeroSelectionOpened = true;
         }
     }
 }
